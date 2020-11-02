85f0e583d964126c64394c98a3ce5b63bf2189f6
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-02 05:59:41 -0500

    Elixir - progetto Basics è iniziato con metodi metodo e funzione

---
+++ basics/lib/basics.ex
@@ -0,0 +1,28 @@
+defmodule Basics do
+  @moduledoc """
+  Documentation for Basics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Basics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  # Implicit return:
+  def metodo do
+    "Ciao! Mi chiamo WMC! Grazie per imparare Elixir come il tuo Linguaggio di Programmazione"
+  end
+
+  def funzione do
+    "Funzione con return"
+  end
+
+end
